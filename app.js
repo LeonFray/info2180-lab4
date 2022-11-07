de97ad94a28040e8b79df0d6a099dbccfa9d47b9
LeonFray <114364472+LeonFray@users.noreply.github.com>
2022-11-06 19:24:04 -0500

    Completed Files

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-function getText(){
-
-
-    var xhr = new XMLHttpRequest();
-    
-    xhr.open('GET','superheroes.php',true)
-    
-    xhr.onload = function(){
-      if (this.status == 200){
-          /*console.log(this.responseText)*/
-    
-          alert(document.getElementById('resText').innerText = this.responseText)
+window.onload = function(){
+  var search = document.querySelector("button");
+  var result = document.querySelector(".result");
+  var query = document.querySelector("input");
+  search.addEventListener('click', handleClick);
+  var httpRequest = new XMLHttpRequest();
+  
+  function handleClick(clickEvent){
+      clickEvent.preventDefault();
+      var url = "superheroes.php?query=" + query.value;
+      httpRequest.onreadystatechange = fetchingdata;
+      httpRequest.open('GET', url, true);
+      httpRequest.send();
+  }
+  function fetchingdata(){
+      if (httpRequest.readyState === XMLHttpRequest.DONE){
+          if (httpRequest.status === 200){
+              var response = httpRequest.responseText;
+              result.innerHTML = response;
+          }
+          else{
+              result.innerHTML = "Error: This resquest can not be deliver. Please try again.";
+          }
       }
-    
-    }
-    
-    xhr.send()
-    }
+  }
+}
